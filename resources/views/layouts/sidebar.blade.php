d82b5e8f1a214c19c081c8f88e6fe917baba025c
thaungpyaephyo <thaungpyaephyo@gmail.com>
2024-02-05 00:14:38 +0630

    first

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,10 @@
+<nav class="mt-10">
+    <x-sidebar-link title="Dashboard"/>
+    <x-sidebar-link title="Team"/>
+    <x-sidebar-link title="Team Member"/>
+    <x-sidebar-link title="Projects"/>
+    <x-sidebar-link title="Tasks"/>
+    <x-sidebar-link title="User"/>
+    <x-sidebar-link title="Role"/>
+    <x-sidebar-link title="Permissions"/>
+</nav>
